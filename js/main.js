0df9135a11f15529e7544f77d4690e2e8c2cab8e
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-09-12 14:13:00 +0530

    Feat: `h3` tag for the restaurant name

--- js/main.js
+++ js/main.js
@@ -153,7 +153,7 @@ createRestaurantHTML = (restaurant) => {
   image.title = restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
